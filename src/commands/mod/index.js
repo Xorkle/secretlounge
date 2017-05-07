c9a8be175dd050db4d6853ad847d0189bd7aca8b
Zack Piper <zack@apertron.net>
2017-05-07 01:55:56 +0100

    uh

--- src/commands/mod/index.js
+++ src/commands/mod/index.js
@@ -4,99 +4,103 @@ const { info } = dude('bot:commands:mod')
 import { sendToAll, sendToUser } from '../../index'
 import { KARMA_PENALTY_WARN } from '../../constants'
 import {
-  cursive, htmlMessage, usersText, handedCooldown, modInfoText, ALREADY_WARNED,
-  MESSAGE_DISAPPEARED, ERR_NO_REPLY
+    cursive, htmlMessage, usersText, handedCooldown, modInfoText, ALREADY_WARNED,
+    MESSAGE_DISAPPEARED, ERR_NO_REPLY
 } from '../../messages'
 import { getFromCache, getCacheGroup, setWarnedFlag, hasWarnedFlag } from '../../cache'
 import {
-  getUser, getUsers,
-  addWarning, rmKarma
+    getUser, getUsers,
+    addWarning, rmKarma
 } from '../../db'
 
 export default function modCommands (user, evt, reply) {
-  const messageRepliedTo = getFromCache(evt, reply)
-  const msgId = evt && evt.raw && evt.raw.reply_to_message && evt.raw.reply_to_message.message_id
+    const messageRepliedTo = getFromCache(evt, reply)
+    const msgId = evt && evt.raw && evt.raw.reply_to_message && evt.raw.reply_to_message.message_id
 
-  switch (evt.cmd) {
+    switch (evt.cmd) {
     case 'modsay':
-      if (evt.args.length <= 0) return reply(cursive('please specify a message, e.g. /modsay message'))
-      info('%o sent mod message -> %s', user, evt.args.join(' '))
-      sendToAll(htmlMessage(evt.args.join(' ') + ' <b>~mods</b>'))
-      break
+        if (evt.args.length <= 0) return reply(cursive('please specify a message, e.g. /modsay message'))
+        info('%o sent mod message -> %s', user, evt.args.join(' '))
+        sendToAll(htmlMessage(evt.args.join(' ') + ' <b>~mods</b>'))
+        break
 
     case 'users':
-      const users = getUsers()
-      reply(htmlMessage(
-        usersText(users, true)
-      ))
-      break
+        const users = getUsers()
+        reply(htmlMessage(
+            usersText(users, true)
+        ))
+        break
 
     case 'info':
-      if (evt && evt.raw && evt.raw.reply_to_message) {
-        if (messageRepliedTo) {
-          const user = getUser(messageRepliedTo.sender)
-          reply(htmlMessage(
-            modInfoText(user)
-          ))
+        if (evt && evt.raw && evt.raw.reply_to_message) {
+            if (messageRepliedTo) {
+                const user = getUser(messageRepliedTo.sender)
+                reply(htmlMessage(
+                    modInfoText(user)
+                ))
+            }
         }
-      }
-      break
+        break
 
     case 'delete':
-      let replyCache = getCacheGroup(msgId)
+        let replyCache = getCacheGroup(msgId)
 
-      if (messageRepliedTo) {
-        if (!hasWarnedFlag(msgId)) {
-          const cooldownTime = addWarning(messageRepliedTo.sender)
-          rmKarma(messageRepliedTo.sender, KARMA_PENALTY_WARN)
-          setWarnedFlag(msgId)
-          getUsers().map((user) => {
-            if (messageRepliedTo.sender !== user.id) {
-              reply({
-                ...cursive(MESSAGE_DISAPPEARED),
-                type: 'editMessageText',
-                chat: user.id,
-                id: replyCache && replyCache[user.id],
-                options: {
-                  parse_mode: 'HTML'
-                }
-              })
+        if (messageRepliedTo) {
+            if (!hasWarnedFlag(msgId)) {
+                const cooldownTime = addWarning(messageRepliedTo.sender)
+                rmKarma(messageRepliedTo.sender, KARMA_PENALTY_WARN)
+                setWarnedFlag(msgId)
+                getUsers().map((user) => {
+                    if (messageRepliedTo.sender !== user.id) {
+                        reply({
+                            ...cursive(MESSAGE_DISAPPEARED),
+                            type: 'editMessageText',
+                            chat: user.id,
+                            id: replyCache && replyCache[user.id],
+                            options: {
+                                parse_mode: 'HTML'
+                            }
+                        })
+                    }
+                })
+                sendToUser(messageRepliedTo.sender, {
+                    ...cursive(handedCooldown(cooldownTime, true)),
+                    options: {
+                        reply_to_message_id: msgId,
+                        parse_mode: 'HTML'
+                    }
+                })
+            } else {
+                reply(cursive(ALREADY_WARNED))
             }
-          })
-          sendToUser(messageRepliedTo.sender, {
-            ...cursive(handedCooldown(cooldownTime, true)),
-            options: {
-              reply_to_message_id: msgId,
-              parse_mode: 'HTML'
-            }
-          })
         } else {
-          reply(cursive(ALREADY_WARNED))
+            reply(cursive(ERR_NO_REPLY))
         }
-      } else {
-        reply(cursive(ERR_NO_REPLY))
-      }
-      break
+        break
 
     case 'warn':
-      if (messageRepliedTo) {
-        if (!hasWarnedFlag(msgId)) {
-          const cooldownTime = addWarning(messageRepliedTo.sender)
-          rmKarma(messageRepliedTo.sender, KARMA_PENALTY_WARN)
-          setWarnedFlag(msgId)
-          sendToUser(messageRepliedTo.sender, {
-            ...cursive(handedCooldown(cooldownTime)),
-            options: {
-              reply_to_message_id: msgId,
-              parse_mode: 'HTML'
+        if (messageRepliedTo) {
+            if (!hasWarnedFlag(msgId)) {
+                const cooldownTime = addWarning(messageRepliedTo.sender)
+                rmKarma(messageRepliedTo.sender, KARMA_PENALTY_WARN)
+                setWarnedFlag(msgId)
+                sendToUser(messageRepliedTo.sender, {
+                    ...cursive(handedCooldown(cooldownTime)),
+                    options: {
+                        reply_to_message_id: msgId,
+                        parse_mode: 'HTML'
+                    }
+                })
+            } else {
+                reply(cursive(ALREADY_WARNED))
             }
-          })
         } else {
-          reply(cursive(ALREADY_WARNED))
+            reply(cursive(ERR_NO_REPLY))
         }
-      } else {
-        reply(cursive(ERR_NO_REPLY))
-      }
-      break
-  }
+        break
+    case 'dbg':
+        console.log(messageRepliedTo.sender);
+        break
+
+    }
 }
